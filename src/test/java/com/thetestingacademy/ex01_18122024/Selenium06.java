4635e28803184eca33fef2c7fbdacae5fd5e0849
praveen-raipuria <praveenraipuria@gmail.com>
2024-12-18 22:31:30 +0530

    18th Dec 2024

---
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium06.java
@@ -0,0 +1,12 @@
+package com.thetestingacademy.ex01_18122024;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Selenium06 {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://sdet.live");
+    }
+}
+
---
+++ src/test/java/com/thetestingacademy/ex01_18122024/Selenium08.java
@@ -0,0 +1,37 @@
+package com.thetestingacademy.ex01_18122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+public class Selenium08 {
+    @Test
+    public void testMethod01(){
+
+        // EdgeOptions -> It will help you set the browser
+        // options to browsers
+        // window - size
+        // headless mode - there is not UI -> advantage - Fast Execution
+        // full UI mode - default - UI browser
+        // incognito mode - switch
+        // start Max
+        // add extensions - browsers
+        // 100 + others , https , http
+        // localstorage, download ?
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+//        edgeOptions.addArguments("--start-maximized");
+        edgeOptions.addArguments("--window-size=1920,1080");
+        edgeOptions.addArguments("--incognito");
+        edgeOptions.addArguments("--headless");
+
+        EdgeDriver driver = new EdgeDriver(edgeOptions);
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+
+
+
+
+    }
+}
+
